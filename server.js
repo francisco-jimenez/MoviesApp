fdd45ff28ade086e6dd8ae09e0838e1ad88ed580
Curro <curro.jimenez.dev@gmail.com>
2020-02-18 15:30:09 +0100

    Change server port

--- server.js
+++ server.js
@@ -49,6 +49,4 @@ app.use(function(err, req, res, next) {
   else 
     res.status(500).json({message: "Something looks wrong :( !!!"});
 });
-app.listen(3000, function(){
- console.log('Node server listening on port 3000');
-});
+app.listen(process.env.PORT || 5000)
