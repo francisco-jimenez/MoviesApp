827f8513d526923fadff0b9daa5ca5b2753f7b1b
Curro <curro.jimenez.dev@gmail.com>
2020-02-18 15:15:34 +0100

    Connected to atlas

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 //Set up mongoose connection
+const MongoClient = require('mongodb').MongoClient;
+
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/node_rest_api';
-mongoose.connect(mongoDB);
+const mongoDBURL = 'mongodb+srv://admin:admin@cluster0-og4sl.gcp.mongodb.net/test?retryWrites=true&w=majority';
+// const client = new MongoClient(mongoDBURL, { useNewUrlParser: true });
+mongoose.connect(mongoDBURL);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
