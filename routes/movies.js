91b52abc73657a362b3d3fd19ba1e5b6919d243e
Curro <curro.jimenez.dev@gmail.com>
2020-02-19 16:32:24 +0100

    Allow access from cross-origin

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const movieController = require('../app/api/controllers/movies');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  });
 router.get('/', movieController.getAll);
 router.post('/', movieController.create);
 router.get('/:movieId', movieController.getById);
